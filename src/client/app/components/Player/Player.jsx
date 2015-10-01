0d08f30d4f1121a8e81479ce440b16152fe38d51
Doug Swain <dswain@gmail.com>
2015-10-01 14:47:16 -0400

    Moved the Player track info below the player itself.

--- src/client/app/components/Player/Player.jsx
+++ src/client/app/components/Player/Player.jsx
@@ -31,14 +31,15 @@ export default class Player extends React.Component {
         
         return (
             <div className={styles.player}>
-                <div className={styles.info}>
-                    <span>Now Playing: </span>
-                    <span className={styles.trackTitle}>{trackTitle}</span>{this.props.displayInfo}
-                </div>
                 <div className={styles.controls}>
                     <span className={styles.button} onClick={this.onTrackEnded.bind(this, TRACK_FINISHED_PREV)}>&#x25C1;</span>
                     <audio className={styles.audio} ref="audioTag" src={startTrack && startTrack.url ? startTrack.url : ""} controls="controls" autoPlay="autoplay"></audio>
                     <span className={styles.button} onClick={this.onTrackEnded.bind(this, TRACK_FINISHED_NEXT)}>&#x25B7;</span>
+                </div>
+                <div className={styles.info}>
+                    <span>Now Playing: </span>
+                    <span className={styles.trackTitle}>{trackTitle}</span>
+                    {this.props.displayInfo}
                 </div>
             </div>
         );
